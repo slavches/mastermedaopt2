2966ada60f9d8d2f0e69233fb752f6e589366a9a
slavches <mbsvyatoslav@gmail.com>
2026-02-09 18:42:47 +0300

    home redact

--- src/components/Sections/Home.jsx
+++ src/components/Sections/Home.jsx
@@ -9,9 +9,9 @@ import OrderForm from './OrderForm';
 
 function Home() {
   const backgroundImages = [
-    "/images/bg-honey-1.webp",
-    "/images/bg-honey-2.webp",
-    "/images/bg-honey-3.webp"
+    "/images/bg-honey-1.png",
+    "/images/bg-honey-2.png",
+    "/images/bg-honey-3.png"
   ];
 
   return (
